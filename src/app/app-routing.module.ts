158b77cd0c84fb8eff3dd34badbeda12f8055cf2
David Zambrano <dzambrano.226@gmail.com>
2021-02-13 10:54:54 -0500

    :saparkles: Initial configuration project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', loadChildren: () => import('./pages/login/login.module').then( m => m.LoginModule) },
+  { path: 'register', loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterModule) },
+  { path: '', loadChildren: () => import('./pages/landing/landing.module').then( m => m.LandingModule) },
+  { 
+    path: '**', 
+    loadChildren: () => import('./pages/page-not-found/page-not-found.module').then( m => m.PageNotFoundModule) 
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
